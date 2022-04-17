3e5fdd0328102f011999327f774b8f555762acf8
sorena <sorenapaydar81@gmail.com>
2022-04-18 01:26:27 +0430

    update: app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 import Discord = require("discord.js");
-const QRCode = require("./QRCode");
+import parser from "./parser";
+import QRCode from "./QRCode";
 
 const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES"],
@@ -11,10 +12,15 @@ client.on("ready", () => {
   console.log(`Bot has been logged in as ${client.user?.tag}`);
 });
 
-client.on("message", async (msg) => {
-  const [command, argument] = msg.content.split(" ");
-  if (command === "qr" && argument) {
-    const qrCode = new QRCode(argument);
+client.on("message", (msg) => {
+  const {
+    command,
+    data,
+    args: { width, height, color },
+  } = parser(msg.content);
+
+  if (command === "qr" && data.length > 0) {
+    const qrCode = new QRCode(data, width, height, color);
     msg.reply(qrCode.getQRCode());
   }
 });
